6cff9355724c5cbd432ad6f378c03c673a43561e
anonymous <anonymous>
2026-05-06 09:47:18 +0800

    init

---
+++ data/data.js
@@ -0,0 +1,80 @@
+const data_list = [
+    {
+        "text":"The lady in the red car is crossing the bridge.",
+        "prompt":"demo/prompt/11_common_voice_en_33907.wav",
+        "ground_truth":"demo/ground_truth/11_common_voice_en_33907-common_voice_en_33908.wav",
+        "semavoice_emilia_ours":"demo/semavoice_emilia_ours/11_common_voice_en_33907-common_voice_en_33908.wav",
+        "semavoice":"demo/semavoice/11_common_voice_en_33907-common_voice_en_33908.wav",
+        "cosyvoice2":"demo/cosyvoice2/11_common_voice_en_33907-common_voice_en_33908.wav",
+        "indextts2":"demo/indextts2/11_common_voice_en_33907-common_voice_en_33908.wav",
+    },
+
+    {
+        "text":"The children of Gowanda had school that day.",
+        "prompt":"demo/prompt/13_common_voice_en_20030172.wav",
+        "ground_truth":"demo/ground_truth/13_common_voice_en_20030172-common_voice_en_20030173.wav",
+        "semavoice_emilia_ours":"demo/semavoice_emilia_ours/13_common_voice_en_20030172-common_voice_en_20030173.wav",
+        "semavoice":"demo/semavoice/13_common_voice_en_20030172-common_voice_en_20030173.wav",
+        "cosyvoice2":"demo/cosyvoice2/13_common_voice_en_20030172-common_voice_en_20030173.wav",
+        "indextts2":"demo/indextts2/13_common_voice_en_20030172-common_voice_en_20030173.wav",
+    },
+
+    {
+        "text":"One variation assigns a point based on the pair rolled, rather than the singleton.",
+        "prompt":"demo/prompt/15_common_voice_en_19724030.wav",
+        "ground_truth":"demo/ground_truth/15_common_voice_en_19724030-common_voice_en_19724029.wav",
+        "semavoice_emilia_ours":"demo/semavoice_emilia_ours/15_common_voice_en_19724030-common_voice_en_19724029.wav",
+        "semavoice":"demo/semavoice/15_common_voice_en_19724030-common_voice_en_19724029.wav",
+        "cosyvoice2":"demo/cosyvoice2/15_common_voice_en_19724030-common_voice_en_19724029.wav",
+        "indextts2":"demo/indextts2/15_common_voice_en_19724030-common_voice_en_19724029.wav",
+    },
+
+    {
+        "text":"I do not like sushi, so I did not really enjoy the meal.",
+        "prompt":"demo/prompt/12_common_voice_en_1416089.wav",
+        "ground_truth":"demo/ground_truth/12_common_voice_en_1416089-common_voice_en_1416093.wav",
+        "semavoice_emilia_ours":"demo/semavoice_emilia_ours/12_common_voice_en_1416089-common_voice_en_1416093.wav",
+        "semavoice":"demo/semavoice/12_common_voice_en_1416089-common_voice_en_1416093.wav",
+        "cosyvoice2":"demo/cosyvoice2/12_common_voice_en_1416089-common_voice_en_1416093.wav",
+        "indextts2":"demo/indextts2/12_common_voice_en_1416089-common_voice_en_1416093.wav",
+    },
+
+    {
+        "text":"这也是英超球队近年来赢得过的最后一次欧冠冠军",
+        "prompt":"demo/prompt/00_00004601-00000061.wav",
+        "ground_truth":"demo/ground_truth/00_00004601-00000038.wav",
+        "semavoice_emilia_ours":"demo/semavoice_emilia_ours/00_00004601-00000038.wav",
+        "semavoice":"demo/semavoice/00_00004601-00000038.wav",
+        "cosyvoice2":"demo/cosyvoice2/00_00004601-00000038.wav",
+        "indextts2":"demo/indextts2/00_00004601-00000038.wav",
+    },
+    {
+        "text":"但强风不仅没把竹架吹走，沙子还顺着风势沉降在护沙围栏间",
+        "prompt":"demo/prompt/07_00004656-00000076.wav",
+        "ground_truth":"demo/ground_truth/07_00004656-00000075.wav",
+        "semavoice_emilia_ours":"demo/semavoice_emilia_ours/07_00004656-00000075.wav",
+        "semavoice":"demo/semavoice/07_00004656-00000075.wav",
+        "cosyvoice2":"demo/cosyvoice2/07_00004656-00000075.wav",
+        "indextts2":"demo/indextts2/07_00004656-00000075.wav",
+    },
+
+    {
+        "text":"收银员用收银机撤销结账两月，偷拿超市九点二万元",
+        "prompt":"demo/prompt/09_00005054-00000084.wav",
+        "ground_truth":"demo/ground_truth/09_00005054-00000113.wav",
+        "semavoice_emilia_ours":"demo/semavoice_emilia_ours/09_00005054-00000113.wav",
+        "semavoice":"demo/semavoice/09_00005054-00000113.wav",
+        "cosyvoice2":"demo/cosyvoice2/09_00005054-00000113.wav",
+        "indextts2":"demo/indextts2/09_00005054-00000113.wav",
+    },
+    
+    {
+        "text":"在茶杯或保温杯中以沸水三百ml左右冲泡",
+        "prompt":"demo/prompt/18_00004938-00000099.wav",
+        "ground_truth":"demo/ground_truth/18_00004938-00000054.wav",
+        "semavoice_emilia_ours":"demo/semavoice_emilia_ours/18_00004938-00000054.wav",
+        "semavoice":"demo/semavoice/18_00004938-00000054.wav",
+        "cosyvoice2":"demo/cosyvoice2/18_00004938-00000054.wav",
+        "indextts2":"demo/indextts2/18_00004938-00000054.wav",
+    },
+]
